e2fcde81eab152cf9761814608345db5c3cbf21e
Toni Mikut <tmikut@gmail.com>
2023-01-31 17:13:26 +0100

    initial commit

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Toni Mikut on 31.01.23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
